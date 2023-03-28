0045387ca5a0f0bfa800f5ab7a8ddcb9e4f6b826
Rbayonagalvis <rbayonagalvis@gmail.com>
2023-03-27 20:42:58 -0500

    tipos de datos

---
+++ Script.js
@@ -0,0 +1,18 @@
+//hola mundo
+var nombre = "ruben";
+console.log(nombre);
+
+//tipos de datos en javascript
+/* 
+En Javascript existen 7 tipos de datos
+*String Cadenas de texto.
+*Number Valores numéricos.
+*Boolean Representa una entidad lógica y puede tener dos 
+valores: true y false.
+*null Es un valor asignado tiene el valor de “no valor”.
+*undefined Una variable a la que no se le ha asignado ningún 
+valor tiene el valor undefined.
+*Symbol Nuevo en ECMAScript 2015.
+*Object Un valor en memoria al que podemos acceder por un 
+identificador.
+*/
