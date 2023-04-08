71596eaa092197b9bf6eedbde30248f7f96a6f8c
Rbayonagalvis <rbayonagalvis@gmail.com>
2023-04-07 23:32:29 -0500

    cadenas y boleanos

---
+++ BOLEANOS/Boleanos.js
@@ -0,0 +1,24 @@
+//*Ejercicio 1
+
+var a = 10;
+var b = 20;
+if (a>b) 
+	console.log("Variable `a` has higher value"); 
+else 
+	console.log("Variable `b` has higher value"); 
+
+
+//*ejercicio 2
+
+var studentName = "John";
+var haveEnrolledInCourse = true;
+var haveCompletedTheCourse = true;
+
+if (haveEnrolledInCourse){ 
+	console.log("Welcome "+studentName+" to Udacity!"); 
+}
+
+if (haveCompletedTheCourse){
+    console.log("felicitaciones "+studentName+" has finalizado el curso de Udacity!");
+}
+
