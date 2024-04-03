7323a8072d728cdc8262e7fc87c5a893d202781e
Rbayonagalvis <rbayonagalvis@gmail.com>
2024-04-03 12:31:16 -0500

    ejercicios

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,9 @@
 //prueba de correr codigo
+
+console.log("javascript es tremendo");
+
+function saludar(nombre) {
+  console.log("hola ${nombre}");
+}
+
+saludar("eduar"); // Se escribe el nombre entre comillas dobles
---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+/* con var declaro variables y con nombre = declaro
+el valor de la variable*/
+var numero;
+
+numero = 7;
+
+console.log (numero);
+/*con console.log typeof conozco el tipo de dato que tiene
+esta variable */
+console.log (typeof(numero));
+
+numero = '7';
+
+console.log(numero);
+
+console.log (typeof(numero));
